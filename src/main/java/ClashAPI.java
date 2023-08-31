c1dce49537a50694d38dc0d3b8dd7063faec9d60
EpicCoco <forthecodeman@gmail.com>
2023-08-30 23:31:17 -0400

    added clashapikey to gitignore

--- src/main/java/ClashAPI.java
+++ src/main/java/ClashAPI.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Scanner;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -12,17 +14,19 @@ import com.google.gson.JsonParser;
 public class ClashAPI {
 	
 	//private final static String apitoken3 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImNiNTQ2NGU3LTBhM2UtNDliYS1iYWExLTNmNWYxODBkYzIzZCIsImlhdCI6MTY5MDMxNTIxOCwic3ViIjoiZGV2ZWxvcGVyLzcyOGIzNzJhLTUzZDgtODIwZC0wNDBmLTA0ZDgyYzc1MzdkMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE5OC4yMzIuMTIzLjE2NiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.HpoL3qQsxzsCf0k9mbhql5bR9ACbXSuvtQKOeswMdlo3Va_OLZT4JAl0E4GpZRZw5jTaNKD-nZjlCqOI2w0TXw";
-	private final static String apitoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQxNTljNmMxLTEyZTYtNDdhZS1hNzZiLTZiMGIyNmI2NDE5ZCIsImlhdCI6MTY5MTM1Njg2NCwic3ViIjoiZGV2ZWxvcGVyLzcyOGIzNzJhLTUzZDgtODIwZC0wNDBmLTA0ZDgyYzc1MzdkMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjM1LjE1MS4yNTAuMTM5Il0sInR5cGUiOiJjbGllbnQifV19.iOYupTmhRTIgLqS1Aa8Xq0i2E9sE0ESZFFxBPttYVnmIJj7JYdbV4tUYXyiRrKtNbBb9hi1o7e1SXOQbdTkwwg";
+	private static String apitoken;
 	// ^^ at home
 	private final static String apiendpoint = "https://api.clashofclans.com/v1";
 	//private final static String restOfUrl = "/clans/%232Q9G8QJ2P/members";
 	private static String playertag = "#P9YJ2QQ89";
 	
+	/*
+	 * I'm pretty sure this method isn't used at all (Was only used for JWT command, now deprecated)
+	 */
 	public static String start() {
 		try {
-			
+			setClashAPIKey();
 			playertag = URLEncoder.encode(playertag, "UTF-8");
-			
 			HttpURLConnection connection = (HttpURLConnection) new URL(apiendpoint + "/players/" + playertag).openConnection();
             connection.setRequestProperty("Accept", "application/json");
             connection.setRequestProperty("authorization", "Bearer " + apitoken);
@@ -78,7 +82,7 @@ public class ClashAPI {
 	} //getClanData
 	
 	public static JsonObject getData(String tag, String type) throws FileNotFoundException {
-		
+		setClashAPIKey();
 		try {
 			HttpURLConnection connection = (HttpURLConnection) new URL(apiendpoint + "/" + type + "/" + tag).openConnection();
 			//System.out.println(connection.toString());
@@ -111,5 +115,16 @@ public class ClashAPI {
 		} //try
 		return new JsonObject();
     } //getData
+
+	public static void setClashAPIKey() {
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("clashapikey"));
+			apitoken = sc.nextLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("Failed to find clashapikey.");
+			e.printStackTrace();
+		} //try
+	} //getClashAPIKey
 	
 } //ClashAPI
